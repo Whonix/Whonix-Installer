9e970d29aa6450e2d8e6d648be24a550d5e13b67
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-11-24 18:39:17 -0600

    Integrate Hyper-V disable script into Whonix-Installer, misc. improvements mostly to error handling

--- build-windows-einsiedler.sh
+++ build-windows-einsiedler.sh
@@ -12,19 +12,19 @@ set -o pipefail
 set -o nounset
 
 ## Only on developer machine for local test builds only.
-export use_ppcross_x64_maybe="--compiler=/usr/bin/ppcrossx64"
-export skip_fpc_windows_dependencies_check="true"
+#export use_ppcross_x64_maybe="--compiler=/usr/bin/ppcrossx64"
+#export skip_fpc_windows_dependencies_check="true"
 
 export TARGET_SYSTEM="WINDOWS"
 
 ## hardcoded
-export VERSION_FULL="Xfce-16.0.9.8"
+export VERSION_FULL="LXQt-18.0.7.0.Intel_AMD64"
 
 export FILE_LICENSE="../deps/license.txt"
 export FILE_WHONIX_OVA="../deps/Whonix-$VERSION_FULL.ova"
 export FILE_WHONIX_STARTER_MSI="../deps/WhonixStarterInstaller.msi"
 export FILE_VCREDIST_INST_EXE="../deps/VC_redist.x64.exe"
-export FILE_VBOX_INST_EXE="../deps/VirtualBox-7.1.0-164728-Win.exe"
+export FILE_VBOX_INST_EXE="../deps/VirtualBox-7.2.4-170995-Win.exe"
 export FILE_INSTALLER_BINARY_FINAL="build/WhonixInstaller-$VERSION_FULL.exe"
 
 ./build.sh
